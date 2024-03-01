a825919e31647b0cf44d22e68cc89983ec28cfb0
Nurbekprog <nurprogdata@gmail.com>
2024-03-01 14:11:22 +0500

    form-section finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Sertificates from "./components/sertificates/Sertificates";
 import Products from "./components/products/Products";
+import Form from "./components/form/Form";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <About />
         <Sertificates />
         <Products />
+        <Form />
       </div>
     );
   }
---
+++ src/components/form/Form.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./Form.scss";
+import formBanner from "../../assets/images/form-banner.png";
+class Form extends Component {
+  render() {
+    return (
+      <div
+        className="form container"
+        style={{ backgroundImage: `url(${formBanner})` }}
+      >
+        <div className="form-title">
+          <h2>Получить подробную </h2>
+          <h2 id="form-title-h22">информацию</h2>
+        </div>
+        <p>
+          Просто заполните форму, наш менеджер свяжется с вами в ближайшее время
+        </p>
+        <form>
+          <input type="text" placeholder="Ваше имя" />
+          <input type="tel" placeholder="+375 (29) 0000000" />
+        </form>
+        <textarea
+          name="form"
+          id="form-note"
+          cols="30"
+          rows="5"
+          placeholder="Комментарий"
+        ></textarea>
+        <button>Получить информацию</button>
+      </div>
+    );
+  }
+}
+export default Form;
